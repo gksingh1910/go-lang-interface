49ed6e61f20edb92dcee0aeace1cddadff234656
gaurav <gauravk@nu10.co>
2023-07-26 14:52:38 +0530

    interface struct and slice

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-/* type calculator interface {
+import "fmt"
+
+type calculator interface {
 	sum() int
 	mul() int
 	countList() int
-} */
+}
 
 type sliceCalculator struct {
 	x []int
@@ -18,6 +20,18 @@ func (sc sliceCalculator) sum() int {
 	return value
 }
 
+func (sc sliceCalculator) mul() int {
+	value := 1
+	for i := 0; i < len(sc.x); i++ {
+		value *= sc.x[i]
+	}
+	return value
+}
+
+func (sc sliceCalculator) countList() int {
+	return len(sc.x)
+}
+
 func callSliceCalculator1() {
 	a := sliceCalculator{
 		x: []int{1, 2, 3, 4, 5},
@@ -25,11 +39,13 @@ func callSliceCalculator1() {
 	print(a.sum())
 }
 
-/* func callSliceCalculator2(c calculator) {
-	print(c.sum())
-} */
+func callSliceCalculator2(c calculator) {
+	fmt.Printf("\nsum of slice is %v", c.sum())
+	fmt.Printf("\nmul of slice is %v", c.mul())
+	fmt.Printf("\ncount of slice is %v", c.countList())
+}
 
 func main() {
 	callSliceCalculator1()
-	//callSliceCalculator2(sliceCalculator{x : []int{2,3,4}})
+	callSliceCalculator2(sliceCalculator{x: []int{2, 3, 4}})
 }
